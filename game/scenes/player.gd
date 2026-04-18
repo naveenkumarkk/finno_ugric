8ecfacc224eab8758203821b43a9016492ff28b8
jjusttan <nuntakarnn.b@gmail.com>
2026-04-18 11:24:59 +0300

    feat:initial commit

---
+++ game/scenes/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	# arrow keys OR W A S D both work
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	
+	# normalize so diagonal isn't faster
+	if direction.length() > 0:
+		direction = direction.normalized()
+	
+	velocity = direction * SPEED
+	move_and_slide()
